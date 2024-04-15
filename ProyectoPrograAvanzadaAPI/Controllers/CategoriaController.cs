4b5446b0cc03efd16f7adb75904990cccaf5a638
Yeikel2022 <catieyeik56@gmail.com>
2024-04-14 21:16:21 -0600

    Aporte sobre el mantenimiento de categorías y productos.
    A continuación se mostrara los archivos modificados:
    
    Controllers:
    =========
    CategoriaController:
    	1. Inclusión del método EliminarCategoria, tanto con su versión HttpGet como su HttpPost.
    
    ProductoController:
    	1. Inclusión del método EliminarProducto, tanto con su versión HttpGet como su HttpPost.

---
+++ ProyectoPrograAvanzadaAPI/Controllers/CategoriaController.cs
@@ -0,0 +1,184 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using ProyectoPrograAvanzadaAPI.Entities;
+using System.Data.SqlClient;
+using System.Data;
+using Dapper;
+
+namespace ProyectoPrograAvanzadaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController(IConfiguration _configuration) : ControllerBase
+    {
+		/*Este sirve para poder hacer la consulta a nivel general, esto con booleano.*/
+		[Authorize]
+		[HttpGet]
+		[Route("ConsultarCategorias")]
+		public IActionResult ConsultarCategorias(bool MostrarTodos)
+		{
+			using (var db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+			{
+				CategoriaRespuesta respuesta = new CategoriaRespuesta();
+
+				var resultado = db.Query<Categoria>("ConsultarCategorias",
+					new { MostrarTodos },
+					commandType: CommandType.StoredProcedure).ToList();
+
+				if (resultado == null)
+				{
+					respuesta.Codigo = "-1";
+					respuesta.Mensaje = "No hay categorias registradas";
+				}
+				else
+				{
+					respuesta.Datos = resultado;
+				}
+
+				return Ok(respuesta);
+			}
+		}
+
+
+		/*Este sirve para poder hacer la consulta a nivel especifico, precisamente el IdCategoria.*/
+		[Authorize]
+		[HttpGet]
+		[Route("ConsultarCategoria")]
+		public IActionResult ConsultarCategoria(long IdCategoria)
+		{
+			using (var db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+			{
+				CategoriaRespuesta respuesta = new CategoriaRespuesta();
+
+				var resultado = db.Query<Categoria>("ConsultarCategoria",
+					new { IdCategoria },
+					commandType: CommandType.StoredProcedure).FirstOrDefault();
+
+				if (resultado == null)
+				{
+					respuesta.Codigo = "-1";
+					respuesta.Mensaje = "No hay categoria registrada";
+				}
+				else
+				{
+					respuesta.Dato = resultado;
+				}
+
+				return Ok(respuesta);
+			}
+		}
+
+
+		/*Este sirve para poder registrar las categorias*/
+		[Authorize]
+		[HttpPost]
+		[Route("RegistrarCategoria")]
+		public IActionResult RegistrarCategoria(Categoria entidad)
+		{
+			using (var db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+			{
+				Respuesta respuesta = new Respuesta();
+
+				var resultado = db.Execute("RegistrarCategoria",
+					new { entidad.Nombre },
+					commandType: CommandType.StoredProcedure);
+
+				if (resultado <= 0)
+				{
+					respuesta.Codigo = "-1";
+					respuesta.Mensaje = "Esta Categoria ya se encuentra registrado";
+				}
+				return Ok(respuesta);
+
+			}
+		}
+
+
+		/*Este sirve para poder actualizar las categorias*/
+		[Authorize]
+		[HttpPut]
+		[Route("ActualizarCategoria")]
+		public IActionResult ActualizarCategoria(Categoria entidad)
+		{
+			using (var db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+			{
+				Respuesta respuesta = new Respuesta();
+
+				var resultado = db.Execute("ActualizarCategoria",
+					new { entidad.IdCategoria, entidad.Nombre },
+					commandType: CommandType.StoredProcedure);
+
+				if (resultado <= 0)
+				{
+					respuesta.Codigo = "-1";
+					respuesta.Mensaje = "No se puede realizar la actualización, intentelo de nuevo.";
+				}
+				return Ok(respuesta);
+
+			}
+		}
+
+
+		[Authorize]
+		[HttpGet]
+		[Route("ConsultarProductosCat")]
+		public IActionResult ConsultarProductosCat(int idcategoria)
+		{
+			try
+			{
+				using (var db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+				{
+					ProductoRespuesta respuesta = new ProductoRespuesta();
+
+					var resultado = db.Query<Producto>("ConsultarProductosCat",
+						new { idcategoria },
+						commandType: CommandType.StoredProcedure).ToList();
+
+					if (resultado == null)
+					{
+						respuesta.Codigo = "-1";
+						respuesta.Mensaje = "No hay productos registrados en esta categoria";
+					}
+					else
+					{
+						respuesta.Datos = resultado;
+					}
+
+					return Ok(respuesta);
+				}
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+
+
+
+		/*Este sirve para poder actualizar las categorias*/
+
+		[HttpDelete]
+		[Route("EliminarCategoria")]
+		public IActionResult EliminarCategoria(long IdCategoria)
+		{
+			using (var db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+			{
+				Respuesta respuesta = new Respuesta();
+
+				var resultado = db.Execute("EliminarCategoria",
+					new { IdCategoria },
+					commandType: CommandType.StoredProcedure);
+
+				if (resultado <= 0)
+				{
+					respuesta.Codigo = "-1";
+					respuesta.Mensaje = "No se puede realizar la eliminación, inténtelo de nuevo.";
+				}
+				return Ok(respuesta);
+
+			}
+		}
+	}
+}
---
+++ ProyectoPrograAvanzadaAPI/Controllers/ProductoController.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using ProyectoPrograAvanzadaAPI.Entities;
+using System.Data.SqlClient;
+using System.Data;
+using Dapper;
+
+namespace ProyectoPrograAvanzadaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController(IConfiguration _configuration) : ControllerBase
+    {
+
+    
+        [HttpGet]
+        [Route("ConsultarProductos")]
+        public IActionResult ConsultarProductos(bool MostrarTodos)
+        {
+            using (var db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                ProductoRespuesta respuesta = new ProductoRespuesta();
+
+                var resultado = db.Query<Producto>("ConsultarProductos",
+                    new { MostrarTodos },
+                    commandType: CommandType.StoredProcedure).ToList();
+
+                if (resultado == null)
+                {
+                    respuesta.Codigo = "-1";
+                    respuesta.Mensaje = "No hay productos registrados";
+                }
+                else
+                {
+                    respuesta.Datos = resultado;
+                }
+
+                return Ok(respuesta);
+            }
+        }
+
+
+
+
+        [Authorize]
+        [HttpGet]
+        [Route("ConsultarProducto")]
+        public IActionResult ConsultarProducto(long IdProducto)
+        {
+            using (var db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                ProductoRespuesta respuesta = new ProductoRespuesta();
+
+                var resultado = db.Query<Producto>("ConsultarProducto",
+                    new { IdProducto },
+                    commandType: CommandType.StoredProcedure).FirstOrDefault();
+
+                if (resultado == null)
+                {
+                    respuesta.Codigo = "-1";
+                    respuesta.Mensaje = "No hay Producto registrados";
+                }
+                else
+                {
+                    respuesta.Dato = resultado;
+                }
+
+                return Ok(respuesta);
+            }
+        }
+
+
+
+
+
+        [Authorize]
+        [HttpPost]
+        [Route("RegistrarProducto")]
+        public IActionResult RegistrarProducto(Producto entidad)
+        {
+            using (var db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                Respuesta respuesta = new Respuesta();
+
+                var resultado = db.Query<Producto>("RegistrarProducto",
+                    new { entidad.Nombre, entidad.Precio, entidad.IdCategoria, entidad.Imagen },
+                    commandType: CommandType.StoredProcedure).FirstOrDefault();
+
+                if (resultado == null)
+                {
+                    respuesta.Codigo = "-1";
+                    respuesta.Mensaje = "Este Producto ya se encuentra registrado";
+                }
+                else
+                {
+                    respuesta.ConsecutivoGenerado = resultado.IdProducto;
+                }
+
+                return Ok(respuesta);
+
+            }
+        }
+
+        [Authorize]
+        [HttpPut]
+        [Route("ActualizarProducto")]
+        public IActionResult ActualizarProducto(Producto entidad)
+        {
+            using (var db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                Respuesta respuesta = new Respuesta();
+
+                var resultado = db.Execute("ActualizarProducto",
+                    new { entidad.IdProducto,entidad.Nombre, entidad.Precio, entidad.IdCategoria, entidad.Estado },
+                    commandType: CommandType.StoredProcedure);
+
+                if (resultado <= 0)
+                {
+                    respuesta.Codigo = "-1";
+                    respuesta.Mensaje = "No se pudo actualizar este servicio";
+                }
+
+                return Ok(respuesta);
+            }
+        }
+
+
+        /*Este sirve para poder actualizar los productos*/
+
+        [HttpDelete]
+        [Route("EliminarProducto")]
+        public IActionResult EliminarProducto(long IdProducto)
+        {
+            using (var db = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                Respuesta respuesta = new Respuesta();
+
+                var resultado = db.Execute("EliminarProducto",
+                    new { IdProducto },
+                    commandType: CommandType.StoredProcedure);
+
+                if (resultado <= 0)
+                {
+                    respuesta.Codigo = "-1";
+                    respuesta.Mensaje = "No se puede realizar la eliminación, inténtelo de nuevo.";
+                }
+                return Ok(respuesta);
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
